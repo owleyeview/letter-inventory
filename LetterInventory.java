c6c0288b0c26041c7a3655806e4cd58a4bfdf1c7
DrpMstr <116312100+DrpMstr@users.noreply.github.com>
2022-10-20 17:31:43 -0700

    Update LetterInventory.java

--- LetterInventory.java
+++ LetterInventory.java
@@ -1,11 +1,11 @@
-
+//made by Cece, Derek, Rashaan, and Nathan
 
 public class LetterInventory {
     public static final int LETTERS = 26;
     private boolean isEmpty;  // tracking if the inventory is empty
     private int totalSize;    // total letters in the inventory
     private int[] letterCount = new int[LETTERS];
- 
+
     // 1 argument constructor.  Given a String, creates a LetterInventory object
     public LetterInventory(String letterString) {
         if (letterString.isEmpty()) {
@@ -16,7 +16,7 @@ public class LetterInventory {
             for (int i = 0; i < letterString.length(); i++) {
                 char character = letterString.charAt(i);
                 // if the character is a letter
-                if (character >= 97 && character <= 122) { 
+                if (character >= 97 && character <= 122) {
                     letterCount[character - 97] = letterCount[character - 97] + 1;
                     totalSize++; // increment the total size of the letter inventory
                 }
@@ -29,28 +29,28 @@ public class LetterInventory {
         this.totalSize = 0;
     }
 
-    // takes a character and returns the number of that character currently in 
+    // takes a character and returns the number of that character currently in
     // the inventory.
-    public int get (char letter) {
+    public int get (char letter) throws IllegalArgumentException {
         letter = Character.toLowerCase(letter);
         if (letter >= 97 && letter <= 122) {
             return letterCount[letter - 97];
         } else {
             return 0;
-            // **** need to make this throw an exception ****
+
         }
     }
 
     // takes a character and an int and sets that characters count
     // to the new value
-    public void set (char letter, int value) {
+    public void set (char letter, int value) throws IllegalArgumentException {
         if (letter >= 97 && letter <= 122) {
             int oldValue = letterCount[letter - 97];
             letterCount[letter - 97] = value;
             totalSize = totalSize + (value - oldValue);
         } else {
             return;
-            //  **** need to make this throw an exception ****
+
         }
     }
 
@@ -69,7 +69,7 @@ public class LetterInventory {
         if (isEmpty) {
             return "[]";
         } else {
-            // **** loop through the inventory array and concatonate a string with characters from each index
+            // **** loop through the inventory array and concatenate (create) a string with characters from each index
             String characters = "";
             for (int i = 0; i < LETTERS; i++) {
                 int n = letterCount[i];
@@ -79,13 +79,13 @@ public class LetterInventory {
                 }
             }
             return "[" + characters + "]";
-        } 
+        }
     }
 
-    /* given a LetterInventory object, returns a new LetterInventory 
-    object that contains the sum of the letters from this LetterInventory 
-    and the given one */ 
-    public LetterInventory add(LetterInventory other) {
+    /* given a LetterInventory object, returns a new LetterInventory
+    object that contains the sum of the letters from this LetterInventory
+    and the given one */
+    public LetterInventory add (LetterInventory other) {
         LetterInventory sum = new LetterInventory();
         if (other.isEmpty()) {
             sum.isEmpty = this.isEmpty;
@@ -102,10 +102,35 @@ public class LetterInventory {
         }
     }
 
-    
-    // public LetterInventory subtract(LetterInventory other) {
+    public LetterInventory subtract (LetterInventory other) {
+        LetterInventory result = new LetterInventory();
+        if (other.isEmpty()) {
+            result.isEmpty = this.isEmpty;
+            result.totalSize = this.totalSize;
+            result.letterCount = this.letterCount;
+            return result;
+        } else {
+            int lettersRemoved = 0;
+            for (int i = 0; i < LETTERS; i++) {//subtracts letters that are the same between both
+                if (this.letterCount[i] - other.letterCount[i] < 0) {
+                    result.letterCount[i] = 0;
+                    lettersRemoved += this.letterCount[i];
+                } else {
+                    result.letterCount[i] = this.letterCount[i] - other.letterCount[i];
+                    lettersRemoved += other.letterCount[i];
+                }
+            }
+            if (lettersRemoved >= this.totalSize) {
+                result.isEmpty = true;
+            } else {
+                result.isEmpty = false;
+            }
+            result.totalSize = this.totalSize - lettersRemoved;
+            return result;
+        }
+    }
 
-
-    // // **** after subtraction, check if the inventory is empty
-    // }
 }
+
+//
+
